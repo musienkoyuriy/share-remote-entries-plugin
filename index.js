c55bbbd9d1e6093ba3c1a3f6eddb47af64115d3c
Yuriy Musienko <musienkoyuriy@gmail.com>
2023-02-10 17:51:03 +0200

    async -> runtime marking

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
+const { validate } = require('schema-utils');
 const extractUrlAndGlobal = require('webpack/lib/util/extractUrlAndGlobal');
 const fetch = require('node-fetch');
-const { validate } = require('schema-utils');
 
 const MFPluginsConstructorNames = [
   'NodeFederationPlugin',
   'ModuleFederationPlugin',
   'UniversalFederationPlugin',
-  'NextFederationPlugin'
+  'NextFederationPlugin',
 ];
 
 const schema = {
   type: 'object',
   properties: {
     host: {
-      type: 'string'
-    }
-  }
+      type: 'string',
+    },
+  },
 };
 
-async function publishEntries(options, entries) {
-  await fetch(`${options.host}/entries`, {
+function publishEntries(options, entries) {
+  fetch(options.host, {
     method: 'post',
     headers: {
-      'Content-type': 'application/json'
+      'Content-type': 'application/json',
     },
-    body: JSON.stringify(entries)
+    body: JSON.stringify(entries),
   });
 }
 
 function normalizeRemoteEntry(remoteEntry) {
   const [key, value] = remoteEntry;
-  return value.startsWith('promise ')
-    ? ['promise', key]
-    : extractUrlAndGlobal(value);
+  return value.startsWith('promise ') ? ['Runtime', key] : extractUrlAndGlobal(value);
 }
 
 class ShareRemoteEntriesPlugin {
@@ -40,22 +38,20 @@ class ShareRemoteEntriesPlugin {
     this.options = options;
     validate(schema, options);
   }
-
   apply(compiler) {
-    const federationPlugin =
-      (compiler.options.plugins || []).find(
-        (plugin) => MFPluginsConstructorNames.includes(plugin.constructor.name)
-      );
+    const federationPlugin = (compiler.options.plugins || []).find(
+      plugin => MFPluginsConstructorNames.includes(plugin.constructor.name),
+    );
 
     if (!federationPlugin) {
-      console.error("No ModuleFederationPlugin(s) found.");
+      console.error('No ModuleFederationPlugin(s) found.');
       return;
     }
 
     const remotes = federationPlugin._options.remotes;
     const entries = Object.entries(remotes)
-      .map(remoteEntry => normalizeRemoteEntry(remoteEntry))
-      .reduce((acc, [url, global]) => ({ ...acc, [global]: url }), {});
+      .map(entry => normalizeRemoteEntry(entry))
+      reduce((acc, [url, global]) => ({ ...acc, [global]: url }), {});
 
     publishEntries(this.options, entries);
   }
